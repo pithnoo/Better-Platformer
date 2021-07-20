9db0ef1146f9dc2e6962652fdf2c9c1559b0367c
pithnoo <62898754+pithnoo@users.noreply.github.com>
2021-07-20 20:19:49 +0100

    basic control variables

--- Platformer Prototype/Assets/Code/PlayerController.cs
+++ Platformer Prototype/Assets/Code/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+    public float jumpSpeed;
+    public Transform groundCheck;
+    private Rigidbody2D rigidbody2D;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void FixedUpdate() {
         
     }
 }
