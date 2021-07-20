ddc6a2e3ee2bf27498e824a5d4e6a5cda3d3f74f
pithnoo <62898754+pithnoo@users.noreply.github.com>
2021-07-20 20:23:57 +0100

    Created ground check

--- Platformer Prototype/Assets/Code/PlayerController.cs
+++ Platformer Prototype/Assets/Code/PlayerController.cs
@@ -7,7 +7,11 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpSpeed;
     public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public bool isGrounded;
     private Rigidbody2D rigidbody2D;
+    private bool falling; 
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
     void FixedUpdate() {
