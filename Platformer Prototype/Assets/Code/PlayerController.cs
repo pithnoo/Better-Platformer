dcb54a0cbc50c7002a33ea904a2ba0953f198fcc
pithnoo <62898754+pithnoo@users.noreply.github.com>
2021-07-20 21:02:12 +0100

    Adjusted Player controls

--- Platformer Prototype/Assets/Code/PlayerController.cs
+++ Platformer Prototype/Assets/Code/PlayerController.cs
@@ -6,26 +6,49 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpSpeed;
+
+
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     public bool isGrounded;
-    private Rigidbody2D rigidbody2D;
+
+
+    private Rigidbody2D rb;
     private bool falling; 
+
+    private float xRaw;
+    private float yRaw;
+    public Vector2 dir;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        xRaw = Input.GetAxisRaw("Horizontal");
+        yRaw = Input.GetAxisRaw("Vertical");
+        dir = new Vector2(xRaw,yRaw).normalized;
+
+        if(Input.GetButtonDown("Jump")){
+            //perform jump
+        }
+
+        if(xRaw > 0f){
+            transform.localScale = new Vector2(1,1);
+        }
+        else{
+            transform.localScale = new Vector2(-1,1);
+        }
     }
 
     void FixedUpdate() {
-        
+        rb.velocity = new Vector2(dir.x * moveSpeed, rb.velocity.y);
     }
 }
